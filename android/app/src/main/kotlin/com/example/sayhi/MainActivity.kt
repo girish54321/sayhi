5a10fc4720528966115df8b4bc24136616e67713
Girish Parate <girishparate41@gmail.com>
2021-11-21 18:15:45 +0530

    code added

---
+++ android/app/src/main/kotlin/com/example/sayhi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sayhi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Say Hii"),
+        ),
+        body: ListView(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [Text("data")],
+              ),
+            )
+          ],
+        ));
+  }
+}
