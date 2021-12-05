f5e05c08205d7441cef8897166edde20a9598fd7
Girish Parate <girishparate41@gmail.com>
2021-12-05 20:37:07 +0530

    update icon

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
