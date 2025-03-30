4fa7dd54f004b4ac0ff74a75b5b7054fe2f8049b
Girish Parate <girishparate@Girishs-MacBook-Pro.local>
2025-03-30 21:55:54 +0530

    Update Theam

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,29 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:sayhi/screen/home_screen.dart';
 
 void main() => runApp(MyApp());
 
-final ThemeData lightTheme = ThemeData(
-    primarySwatch: Colors.green,
-    brightness: Brightness.light,
-    useMaterial3: false);
-
-final ThemeData darkTheme = ThemeData(
-    primarySwatch: Colors.green,
-    brightness: Brightness.dark,
-    scaffoldBackgroundColor: Colors.black,
-    useMaterial3: false);
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      home: const HomeScreen(),
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        return MaterialApp(
+          themeMode: ThemeMode.system,
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            colorScheme: darkDynamic,
+            scaffoldBackgroundColor: Colors.black,
+          ),
+          theme: ThemeData(
+            brightness: Brightness.light,
+            colorScheme: lightDynamic,
+          ),
+          title: 'Flutter Demo',
+          home: const HomeScreen(),
+        );
+      },
     );
   }
 }
