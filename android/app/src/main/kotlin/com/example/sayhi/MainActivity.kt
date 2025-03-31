49878997c54f72a0f2768329fc036cd27ed89a73
Girish Parate <girishparate@Girishs-MacBook-Pro.local>
2025-03-31 09:03:59 +0530

    update

--- android/app/src/main/kotlin/com/example/sayhi/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sayhi/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.sayhi
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+    }
 }
